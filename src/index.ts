5bd3e76b79f213aed719de3f6cf7fa359d385fde
Alexander Voronenko <796546@gmail.com>
2023-01-11 08:30:33 +0300

    feat: modify READMI.md

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import 'dotenv/config';
 
 const host = 'localhost';
-const port = 8000;
+const port = Number(process.env.PORT) || 8000;
 
-const server = createServer(
-  (request: IncomingMessage, response: ServerResponse) => {
-    response.end('Hello world!');
-  },
-);
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+  switch (request.method) {
+    case 'GET':
+      console.log(request.method);
+      break;
+    case 'POST':
+      console.log(request.method);
+      break;
+    case 'PUT':
+      console.log(request.method);
+      break;
+    case 'DELETE':
+      console.log(request.method);
+      break;
+
+    default:
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'application/json');
+      response.write(JSON.stringify({ title: 'Not Found', message: 'Route not found' }));
+      response.end();
+      break;
+  }
+
+  response.end('Hello world!');
+});
 
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
